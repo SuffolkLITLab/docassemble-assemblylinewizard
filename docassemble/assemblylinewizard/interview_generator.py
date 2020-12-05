8b48a7177cf43c667032fe427cd10dbacf1bacb7
System Administrator <qsteenhuis@gmail.com>
2020-12-04 21:56:20 -0500

    Search for variables that look like people

--- docassemble/assemblylinewizard/interview_generator.py
+++ docassemble/assemblylinewizard/interview_generator.py
@@ -22,7 +22,7 @@ from .generator_constants import generator_constants
 
 TypeType = type(type(None))
 
-__all__ = ['Playground', 'PlaygroundSection', 'indent_by', 'varname', 'DAField', 'DAFieldList', 'DAQuestion', 'DAInterview', 'DAAttachmentList', 'DAAttachment', 'to_yaml_file', 'base_name', 'to_package_name', 'oneline', 'DAQuestionList', 'map_names', 'is_reserved_label', 'fill_in_field_attributes', 'attachment_download_html', 'get_fields','fill_in_docx_field_attributes','is_reserved_docx_label','get_character_limit']
+__all__ = ['Playground', 'PlaygroundSection', 'indent_by', 'varname', 'DAField', 'DAFieldList', 'DAQuestion', 'DAInterview', 'DAAttachmentList', 'DAAttachment', 'to_yaml_file', 'base_name', 'to_package_name', 'oneline', 'DAQuestionList', 'map_names', 'is_reserved_label', 'fill_in_field_attributes', 'attachment_download_html', 'get_fields','fill_in_docx_field_attributes','is_reserved_docx_label','get_character_limit','get_person_variables']
 
 always_defined = set(["False", "None", "True", "dict", "i", "list", "menu_items", "multi_user", "role", "role_event", "role_needed", "speak_text", "track_location", "url_args", "x", "nav", "PY2", "string_types"])
 replace_square_brackets = re.compile(r'\\\[ *([^\\]+)\\\]')
@@ -1093,39 +1093,23 @@ def should_be_stringified(var_name):
   is_docket_number = var_name.startswith("docket_numbers[")
   return has_no_attributes and not is_docket_number
 
-def get_person_variables(fieldslist):
+def get_person_variables(fieldslist, people_vars=generator_constants.PEOPLE_VARS, people_suffixes = generator_constants.PEOPLE_SUFFIXES):
   """
   Identify the field names that represent people in the list of
   DAFields pulled from docx/PDF.    
   """
   people = set()
   for field in fieldslist:
-    if is_person(field.variable):
-      people.add(get_person_identifier(field.variable))
+    if remove_string_wrapper(field.variable) in people_vars:
+      people.add(field.variable)
+    if '[' in field.variable or '.' in field.variable:
+      match_with_brackets_or_attribute = r"(\D\w*)((\[.*)|(\..*))"
+      matches = re.match(match_with_brackets_or_attribute, field.variable)
+      if matches:
+        # Is the name before attribute/index a predetermined person?  
+        if matches.groups()[0] in people_vars:
+          people.add(matches.groups()[0])
+        else:
+          if matches.groups()[1] in people_suffixes:
+            people.add(matches.groups()[0])
   return people
-      
-def is_person(field_name, people_vars=generator_constants.PEOPLE_VARS):
-  """
-  Check if the field name appears to represent a person
-  """
-  # Is it exactly a person variable: e.g., `users`
-  if remove_string_wrapper(field_name) in people_vars:
-    return True
-  if '[' in field_name or '.' in field_name:
-    match_with_brackets_or_attribute = r"(\D\w*)((\[.*)|(\..*))"
-    matches = re.match(match_with_brackets_or_attribute, field_name)
-    if matches:
-      if matches.groups()[0] in people_vars:
-        return True
-      else:
-        endings = [
-          "[0].address_block()",
-          "[0].address.block()",
-          "[0].address.on_one_line()",
-          "[0].",
-        ]
-        if matches.groups()[1] in endings:
-            return True
-  
-def get_person_identifier(field_name):
-  pass
